a67090939710870405e9ee5812fcd0d8ca9dc688
Kenneth Endfinger <kaendfinger@gmail.com>
2014-07-29 20:16:48 -0400

    Update Build System and Dependencies

--- tool/build.dart
+++ tool/build.dart
@@ -4,7 +4,7 @@ import "dart:io";
 
 var packages_dir = new Directory("packages/");
 
-main(List<String> args) {
+void main(List<String> args) {
   var future = new Future.value(null);
 
   if (!packages_dir.existsSync()) {
@@ -13,11 +13,11 @@ main(List<String> args) {
 
   future.then((_) {
     var argz = (args.length > 0 ? " " : "") + args.join(" ");
-    return execute("dart tool/hop_runner.dart --color${argz}");
+    return execute("dart --checked tool/hop_runner.dart --color${argz}");
   });
 }
 
-execute(String cmdline) {
+dynamic execute(String cmdline) {
   var split = cmdline.split(" ");
   var command = split[0];
   split.remove(command);
@@ -25,7 +25,6 @@ execute(String cmdline) {
   return Process.start(command, args).then((Process process) {
     stdout.addStream(process.stdout);
     stderr.addStream(process.stderr);
-    process.stdin.addStream(stdin);
     return process.exitCode;
   }).then((int exitCode) {
     if (exitCode != 0) {
--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -11,15 +11,16 @@ part 'docgen.dart';
 part 'utils.dart';
 part 'version.dart';
 part 'analyze.dart';
-
-var files = ["lib/json2csv.dart"];
+part 'config.dart';
 
 void main(List<String> args) {
-  addTask("docs", createDocGenTask(".", out_dir: "out/docs"));
-  addTask("analyze", createAnalyzerTask(files));
+  init();
+  addTask("docs", createDocGenTask(".", out_dir: parse_config_value(getvar("docs.output"))));
+  addTask("analyze", createAnalyzerTask(getvar("analyzer.files").map(parse_config_value)));
   addTask("version", createVersionTask());
   addTask("publish", createProcessTask("pub", args: ["publish", "-f"], description: "Publishes a New Version"), dependencies: ["version"]);
   addTask("bench", createBenchTask());
-  addChainedTask("check", ["analyze"]);
+  addTask("test", createProcessTask("dart", args: ["--checked", getvar("test.file")], description: "Runs Unit Tests"));
+  addChainedTask("check", getvar("check.tasks").map(parse_config_value).toList(), description: "Runs the Dart Analyzer and Unit Tests");
   runHop(args);
 }
